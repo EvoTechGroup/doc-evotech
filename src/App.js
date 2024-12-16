e75fbf015652a11ca3f76205020bcc196476d204
huy-lv <huylv177@gmail.com>
2024-12-16 17:02:57 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <iframe
+        src="https://evotech-1.gitbook.io/evotech-docs"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+        title="ig"
+      />
+    </div>
+  );
+}
+
+export default App;
